10c1383f65e63c767a2bce71546d0d340ebb5564
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-07 16:17:24 -0800

    Update with new links

---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <main className="main">
+      <h3 className="main-title">CONTACT</h3>
+    </main>
+  );
+}
---
+++ src/pages/Guides.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Guides() {
+  return (
+    <main className="main">
+      <h3 className="main-title">
+        Learn what items can be composted or recycled!{" "}
+      </h3>
+      <p className="main-body-content">
+        Find out how to properly dispose of your items below.
+      </p>
+    </main>
+  );
+}
