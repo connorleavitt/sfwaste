3625918a264e881383aa4867d8108c0e8d02416c
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-07 16:15:35 -0800

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Header from "./pages/Header";
 import Main from "./pages/Main";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Guides from "./pages/Guides";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <div className="page-container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/search" element={<Main />}></Route>
+          <Route path="/guides" element={<Guides />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
