4893d1dc44a17b78642a9873b1fb403918c01ee5
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-07 17:05:21 -0800

    Add guide and signage content

--- src/pages/Guides.js
+++ src/pages/Guides.js
@@ -4,10 +4,217 @@ export default function Guides() {
   return (
     <main className="main">
       <h3 className="main-title">PRINTABLE GUIDES & SIGNAGE</h3>
-      <div className="guide-container">
-        <div className="compost-guide-container"></div>
-        <div className="recycle-guide-container"></div>
-        <div className="landfill-guide-container"></div>
+      <div className="main-guide-container">
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">Compost</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_compost.pdf">
+            <img
+              className="guides_preview_img"
+              alt="compost guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2016/10/compost-1.png"
+            ></img>
+          </a>
+
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_compost.pdf">
+                Compost Poster 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_posters_85x11_vertical_green_only.pdf">
+                Double-Sided Flyer 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_posters_11x17_vertical_greenonly.pdf">
+                Double-Sided Flyer 11×17
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://sfrecycles.org/recycling-signmaker/compost">
+                SF Environment Sign Maker
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">Recycle</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_recycle.pdf">
+            <img
+              className="guides_preview_img"
+              alt="recycle guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2016/10/recycle.jpg"
+            ></img>
+          </a>
+
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_recycle.pdf">
+                Recycle Poster 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2021/09/sfe_zw_posters_85x11_vertical_trilingual_noFabric_recycle.pdf">
+                Double-Sided Flyer 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_posters_11x17_vertical_blueonly.pdf">
+                Double-Sided Flyer 11×17
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://sfrecycles.org/signmaker">
+                SF Environment Sign Maker
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">Landfill</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_landfill.pdf">
+            <img
+              className="guides_preview_img"
+              alt="landfill guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2016/10/landfill-1.png"
+            ></img>
+          </a>
+
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/85x11_horiz_withexcludes_landfill.pdf">
+                Landfill Poster 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_posters_85x11_vertical_black_only.pdf">
+                Double-Sided Flyer 8.5×11
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_posters_11x17_vertical_blackonly.pdf">
+                Double-Sided Flyer 11×17
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://sfrecycles.org/recycling-signmaker/landfill">
+                SF Environment Sign Maker
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">Tri-Fold</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_educational_trifold_85x11-2.pdf">
+            <img
+              className="guides_preview_img"
+              alt="tri-fold guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_educational_trifold_85x11_BlueAlternative-1-232x300.jpg"
+            ></img>
+          </a>
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_educational_trifold_85x11-2.pdf">
+                English
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_educational_trifold_85x11_ES.pdf">
+                Spanish
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/sfe_zw_educational_trifold_85x11_ZH.pdf">
+                Chinese
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">For Property Owners</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2016/10/Your_Green_cart_is_Now_Here_Poster.pdf">
+            <img
+              className="guides_preview_img"
+              alt="green cart guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2016/10/Property-Managers-223x300.jpg"
+            ></img>
+          </a>
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/TableTent2017_BrandName7460.pdf">
+                Table Tents-Recycling & Composting Sorting Guide
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/NoGlassFlyerFINALBackOnly.pdf">
+                No Glass Flyer
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/NoGlassSticker3x8.5StopSign.jpg">
+                No Glass Graphic
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/Your_Green_cart_is_Now_Here_Poster.pdf">
+                Green Cart is Now Here
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">Other Programs</h3>
+          <a href="https://www.recology.com/recology-san-francisco/sorting-guides-signage/">
+            <img
+              className="guides_preview_img"
+              alt="other program guide pdf"
+              src="https://www.recology.com/wp-content/uploads/2018/09/sfe_zw_batteries_poster_11x17-194x300.jpg"
+            ></img>
+          </a>
+          <ul>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2018/09/sfe_zw_batteries_poster_11x17.jpg">
+                Battery Poster
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2016/10/Bulky_Item_Collection-Bulky_Item_Program_Poster.pdf">
+                Bulky Item Recycling Poster
+              </a>
+            </li>
+            <li className="pdf_list_item">
+              <a href="https://www.recology.com/wp-content/uploads/2022/01/Self-Assessment-Form-Fillable.pdf">
+                Recology Customer Self Assessment Form
+              </a>{" "}
+              (
+              <a href="https://www.recology.com/wp-content/uploads/2022/01/Self-Assessment-Form-Spanish-Fillable.pdf">
+                Spanish version
+              </a>{" "}
+              &{" "}
+              <a href="https://www.recology.com/wp-content/uploads/2022/01/Self-Assessment-Form-Chinese-Fillable.pdf">
+                Chinese version
+              </a>
+              )
+            </li>
+          </ul>
+        </div>
+
+        <div className="subgrid-guides-container">
+          <h3 className="guides-subtitle">PPE Signage</h3>
+          <a href="https://www.recology.com/wp-content/uploads/2020/11/PPE_sfe_zw_posters_black_85x11_horiz.pdf">
+            <img
+              className="guides_preview_img"
+              alt="ppe signage pdf"
+              src="https://www.recology.com/wp-content/uploads/2020/11/SF-PPE-Signage-300x232.png"
+            ></img>
+          </a>
+        </div>
       </div>
     </main>
   );
