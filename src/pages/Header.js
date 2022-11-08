fb9eba9dae0710e4a12e5b52554b7809b8e0b226
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-07 16:16:25 -0800

    Add navbar links

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+
 // import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <h1 className="header-title">What Goes Where?</h1>
+      <Navbar />
     </header>
   );
 }
---
+++ src/pages/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CustomLink from "../components/CustomLink";
+
+export default function Navbar(props) {
+  return (
+    <nav className="nav">
+      <ul>
+        <CustomLink to="/home">Home</CustomLink>
+        <CustomLink to="/search">Search</CustomLink>
+        <CustomLink to="/guides">Guides</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
+      </ul>
+    </nav>
+  );
+}
