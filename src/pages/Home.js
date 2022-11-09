739ba2e27632c823c4bc4159e28251136d753234
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-11-09 12:04:15 -0800

    Add copy content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,64 @@ export default function Home() {
       <main className="main">
         <div className="main-hero">
           <div className="main-hero-content-wrapper">
-            <h2 className="main-home-title">
+            <h2 className="main-hero-title">
               How should you REALLY be disposing of that item?
             </h2>
-            <p className="main-home-body-content">
-              Learn what items can be composted or recycled! Discover what the
-              city of San Franciso (Recology) allows you to compost and recycle.
-              Check with your local Recology center for more information.
+            <p className="main-hero-body-content">San Franciso (Recology)</p>
+          </div>
+        </div>
+        <div className="main-info">
+          <div className="main-info-content-wrapper">
+            <h2 className="main-info-title">
+              Compost, Recycling, Landfill or Other
+            </h2>
+            <h3 className="main-info-title">Why it's important!</h3>
+            <p className="main-info-body-content">
+              Learn what items can be composted or recycled with the SFWaste web
+              app! Discover what the city of San Francisco (Recology) allows you
+              to compost and recycle. Check with your local Recology center for
+              more information.
+              <br></br>
+              <br></br>
+              The environmental impacts of our society's waste play a huge part
+              on our planet's health. Waste in incinerators and landfills create
+              greenhouse gas emissions. Composting and recycling are an
+              effective way to reduce greenhouse gasses. Recycling and
+              composting require little or no investment and are accessible,
+              easy actions for everyone.<sup>1</sup>
             </p>
+            <div className="info-dropdown-container">
+              <img
+                src="https://www.recology.com/wp-content/uploads/2016/10/CompostBin.jpg"
+                alt="compost bin"
+              ></img>
+              <h4>COMPOST</h4>
+              <p>
+                Food scraps, soiled paper, & yard trimmings go in the compost
+                cart - this includes anything from coffee grounds and napkins,
+                to spoiled foods and peels.
+              </p>
+              <img
+                src="https://www.recology.com/wp-content/uploads/2016/10/RecycleBin.jpg"
+                alt="recycle bin"
+              ></img>
+              <h4>RECYCLE</h4>
+              <p>
+                Paper, cardboard, glass bottles, aluminum/tin cans, hard
+                plastics, bundled plastic bags & thin (film) plastics go into
+                the recyclables cart.
+              </p>
+              <img
+                src="https://www.recology.com/wp-content/uploads/2016/10/LandfillBin.jpg"
+                alt="landfill bin"
+              ></img>
+              <h4>LANDFILL</h4>
+              <p>
+                What is trash? Not much! Cat litter, ceramics, & broken glass to
+                name a few. The fewer of these items placed in the landfill cart
+                the better.
+              </p>
+            </div>
           </div>
         </div>
 
@@ -43,6 +93,14 @@ export default function Home() {
             src={GuideImage}
             alt="guides"
           ></img>
+        </div>
+        <div className="footnotes-container">
+          <p className="footnote-content">
+            <sup>1</sup> Environmental Benefits of Recycling and Composting,
+            <a href="https://eurekarecycling.org/wp-content/uploads/2019/08/composting_factsheet_0.pdf">
+              https://eurekarecycling.org/wp-content/uploads/2019/08/composting_factsheet_0.pdf
+            </a>
+          </p>
         </div>
       </main>
     </div>
